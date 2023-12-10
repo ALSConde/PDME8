6c7c0a94c861c9bccfb453ef86137d24cf494e2f
André Conde <andre.conde@aluno.ifsp.edu.br>
2023-12-10 10:18:44 -0300

    Feat: Comment and Company Routers

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from routers.v1.CountryRouter import CountryRouter
 from routers.v1.StateRouter import StateRouter
 from routers.v1.UserRouter import UserRouter
 from routers.v1.JobRouter import JobRouter
+from routers.v1.CompanyRouter import CompanyRouter
+from routers.v1.CommentRouter import CommentRouter
 
 # Application Environment Configuration
 env = get_environment_variables()
@@ -26,6 +28,8 @@ app.include_router(CountryRouter)
 app.include_router(StateRouter)
 app.include_router(CityRouter)
 app.include_router(JobRouter)
+app.include_router(CompanyRouter)
+app.include_router(CommentRouter)
 
 # Initialise Data Model Attributes
 init()
